34630fbbfcfe71bddf1721fb7da0af326f2cdd78
Carl Morris <carl.morris.world@outlook.com>
2019-12-04 06:45:56 +0800

    Files generated by PkgTemplates

---
+++ src/ShellFunctions.jl
@@ -0,0 +1,5 @@
+module ShellFunctions
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ShellFunctions
+using Test
+
+@testset "ShellFunctions.jl" begin
+    # Write your own tests here.
+end
